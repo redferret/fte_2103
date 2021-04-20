8517236d3829b139bfa44d7765ba3abe2cc0efeb
Richard <rdesilvey@gmail.com>
2021-04-20 14:17:02 -0600

    Test for attributes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -7,5 +7,15 @@ describe Item do
       item = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
       expect(item).to be_an Item
     end
+
+    it 'has a name' do
+      item = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      expect(item.name).to eq 'Peach Pie (Slice)'
+    end
+
+    it 'has a price' do
+      item = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      expect(item.price).to eq 3.75
+    end
   end
 end
