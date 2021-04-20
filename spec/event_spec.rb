223262df061c72b2349fe4f56292c4185dabf97e
Richard <rdesilvey@gmail.com>
2021-04-20 14:36:12 -0600

    Test attributes

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,5 +7,15 @@ describe Event do
       event = Event.new('Name of event')
       expect(event).to be_an Event
     end
+
+    it 'has a name' do
+      event = Event.new('Name of event')
+      expect(event.name).to eq 'Name of event'
+    end
+
+    it 'has food trucks' do
+      event = Event.new('Name of event')
+      expect(event.food_trucks).to eq []
+    end
   end
 end
